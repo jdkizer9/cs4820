d66ae6a850ab1ffb8e02b431a58f9f68675b5e71
James <james.kizer@gmail.com>
2014-06-06 12:58:52 -0400

    Code cleanup

--- CS4820p1/CS4820p1/huffman.cpp
+++ CS4820p1/CS4820p1/huffman.cpp
@@ -11,22 +11,12 @@
 #include <unordered_map>
 #include <assert.h>
 
-
-
-
-
 class HuffmanNode
 {
 public:
     
     HuffmanNode(char c);
-    //HuffmanNode(std::string &str, int numRefs);
     HuffmanNode(HuffmanNode *node1, HuffmanNode *node2);
-    //HuffmanNode();
-//    // Copy constructor.
-//    HuffmanNode(const HuffmanNode &);
-//    // Assignment.
-//    HuffmanNode &operator=(const HuffmanNode &);
     
     ~HuffmanNode();
     
@@ -58,16 +48,6 @@ private:
     bool isLeaf;
 };
 
-
-//HuffmanNode::HuffmanNode()
-//{
-//    character = '\0';
-//    numberOfReferences = 0;
-//    prefixCode = nullptr;
-//    leftNode = nullptr;
-//    rightNode = nullptr;
-//}
-
 HuffmanNode::HuffmanNode(char c) : isLeaf(true)
 {
     character = c;
@@ -76,16 +56,6 @@ HuffmanNode::HuffmanNode(char c) : isLeaf(true)
     leftNode = nullptr;
     rightNode = nullptr;
 }
-
-//HuffmanNode::HuffmanNode(std::string &str, int numRefs) : isLeaf(false), numberOfReferences(numRefs)
-//{
-//    character = '\0';
-//    stringOfChars = str;
-//    numberOfReferences = 0;
-//    prefixCode = nullptr;
-//    leftNode = nullptr;
-//    rightNode = nullptr;
-//}
 
 
 HuffmanNode::HuffmanNode(HuffmanNode *node1, HuffmanNode *node2) : isLeaf(false)
@@ -192,22 +162,6 @@ struct CompareNode {
     }
 };
 
-//class mycomparison
-//{
-//    bool reverse;
-//public:
-//    mycomparison(const bool& revparam=false)
-//    {reverse=revparam;}
-//    bool operator() (const HuffmanNode& leftNode, const HuffmanNode&rightNode) const
-//    {
-//        int lhs = leftNode.getNumberOfReferences();
-//        int rhs = rightNode.getNumberOfReferences();
-//        
-//        if (reverse) return (lhs>rhs);
-//        else return (lhs<rhs);
-//    }
-//};
-
 class Huffman
 {
 public:
@@ -229,7 +183,6 @@ void Huffman::setPrefixCode(HuffmanNode *node, std::string &code)
     if(node->isLeafNode())
     {
         node->setPrefixCode(code);
-        //std::cout<<"Setting prefix code of Node " << node->getCharacter() << " to " << node->getPrefixCode()<<std::endl;
         return;
     }
     std::string *newCode = new std::string(code);
@@ -253,7 +206,6 @@ void Huffman::setPrefixCodes()
 
 Huffman::Huffman(std::string &huffmanString)
 {
-    //std::cout << "The Huffman Encoding string is: " << huffmanString << std::endl;
     nodeMap = new std::unordered_map<char, HuffmanNode*>;
     
     //iterate throught the characters in the huffmanString
@@ -266,7 +218,6 @@ Huffman::Huffman(std::string &huffmanString)
             //if there is no node in the map for this character, instantiate one
             huffmanNode = new HuffmanNode(c);
             (*nodeMap)[c] = huffmanNode;
-            //std::cout<< "Creating new node for " << c << std::endl;
         }
         
         int characterReferences = huffmanNode->getNumberOfReferences();
@@ -277,14 +228,9 @@ Huffman::Huffman(std::string &huffmanString)
     
     for (auto it = nodeMap->begin(); it != nodeMap->end(); ++it )
     {
-        //char c = it->first;
         HuffmanNode *node = it->second;
-        //std::cout << "Adding node " << c << ":" << node->getNumberOfReferences() << std::endl;
         //add nodes to the priority queue
         prioQueue->push(node);
-        //node = prioQueue->top();
-        //char prioNodeC = node->getCharacter();
-        //std::cout << "Node at top  "<< prioNodeC << std::endl;
     }
 
     //we now have all our entries counted and added to min prio queue
@@ -293,26 +239,8 @@ Huffman::Huffman(std::string &huffmanString)
     {
         size_t originalSize = prioQueue->size();
         HuffmanNode *node1 = prioQueue->top();
-        
-        
-//        std::cout<<"Node 1 is ";
-//        if (node1->isLeafNode())
-//            std::cout<< node1->getCharacter();
-//        else
-//            std::cout<<node1->getStringOfCharacters();
-//        std::cout<<" with weight " <<node1->getNumberOfReferences()<<std::endl;
-        
         prioQueue->pop();
         HuffmanNode *node2 = prioQueue->top();
-        
-        
-//        std::cout<<"Node 2 is ";
-//        if (node2->isLeafNode())
-//            std::cout<< node2->getCharacter();
-//        else
-//            std::cout<<node2->getStringOfCharacters();
-//        std::cout<<" with weight " <<node2->getNumberOfReferences()<<std::endl;
-        
         
         prioQueue->pop();
         
@@ -320,13 +248,6 @@ Huffman::Huffman(std::string &huffmanString)
         
         //create a new node for these two nodes
         HuffmanNode *newNode = new HuffmanNode(node1, node2);
-
-//        std::cout<<"New Node is ";
-//        if (newNode->isLeafNode())
-//            std::cout<< newNode->getCharacter();
-//        else
-//            std::cout<<newNode->getStringOfCharacters();
-//        std::cout<<" with weight " <<newNode->getNumberOfReferences()<<std::endl;
         
         prioQueue->push(newNode);
         
@@ -340,23 +261,12 @@ Huffman::Huffman(std::string &huffmanString)
     prioQueue->pop();
     assert(prioQueue->size() == 0);
     
-    
-    
     rootNode = new HuffmanNode(node1, node2);
-    
-//    std::cout<<"Root Node is ";
-//    if (rootNode->isLeafNode())
-//        std::cout<< rootNode->getCharacter();
-//    else
-//        std::cout<<rootNode->getStringOfCharacters();
-//    std::cout<<" with weight " <<rootNode->getNumberOfReferences()<<std::endl;
     
     //we now have a proper huffman tree
     //search the tree for leaves and populate prefix codes
-    
+
     setPrefixCodes();
-    
-    //std::cout << std::endl;
 }
 
 Huffman::~Huffman()
@@ -379,8 +289,6 @@ void Huffman::encode(std::string &stringToEncode, std::string &encodedString)
         char c = *it;
         encodedString.append(prefixCodeForChar(c));
     }
-    
-    //std::cout<<"The encoded string for " <<stringToEncode<< " is "<<encodedString <<std::endl;
 }
 
 
@@ -403,8 +311,6 @@ int main(int argc, const char * argv[])
     
     Huffman h(huffmanEncodingString);
     
-    //std::cout << N << " total strings to encode" << std::endl;
-    
     auto it = stringsToEncode.begin();
     int i = 0;
     while (it != stringsToEncode.end())
@@ -413,15 +319,7 @@ int main(int argc, const char * argv[])
         std::string encodedString;
         h.encode(*it++, encodedString);
         std::cout<<encodedString.length()<<std::endl;
-        //std::cout << i << ") " << encodedString <<std::endl;
     }
-    
-    
-    
-    
-    
-    
-    //std::cout << inputString <<" "<< inputString << std::endl;
     
     return 0;
 }
